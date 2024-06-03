fdce41f64368d7cb0a596233caa7dc73091dfb5b
ErenUnal24 <bilalunal24@gmail.com>
2024-06-03 15:22:20 +0300

    Initial Commit

---
+++ FirebaseExercise/FirebaseExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseExerciseApp.swift
+//  FirebaseExercise
+//
+//  Created by Eren on 3.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
